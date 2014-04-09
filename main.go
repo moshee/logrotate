1b37dbe1edfb5ce9de9eb97a737a73456f5e5e3d
moshee <moshee@displaynone.us>
2014-04-09 11:29:32 +0000

    init

---
+++ main.go
@@ -0,0 +1,45 @@
+// Command logrotate writes and rotates logs read from stdin.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/moshee/logrotate/rotator"
+)
+
+var (
+	flagT = flag.Bool("t", false, "Behave like tee(1)")
+	flagC = flag.Int("c", 5000, "Max (uncompressed) logfile size in kB")
+)
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix(os.Args[0] + ": ")
+
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: <process that outputs to stdout> | logrotate [-t] <filename>")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+}
+
+func main() {
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	r, err := rotator.New(os.Stdin, flag.Arg(0), int64(*flagC), *flagT)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	if err := r.Run(); err != nil {
+		log.Print(err)
+		return
+	}
+}
---
+++ rotator/rotator.go
@@ -0,0 +1,118 @@
+// package rotator implements a simple logfile rotator. Logs are read from an
+// io.Reader and are written to a file until they reach a specified size. The
+// log is then gzipped to another file and truncated.
+package rotator
+
+import (
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+// A Rotator reads log lines from an input source and writes them to a file,
+// splitting it up into gzipped chunks once the filesize reaches a certain
+// threshold.
+type Rotator struct {
+	size      int64
+	threshold int64
+	filename  string
+	in        *bufio.Scanner
+	out       *os.File
+	tee       bool
+}
+
+// New returns a new Rotator that is ready to start rotating logs from its
+// input.
+func New(in io.Reader, filename string, thresholdKB int64, tee bool) (*Rotator, error) {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Rotator{
+		size:      stat.Size(),
+		threshold: 1000 * thresholdKB,
+		filename:  filename,
+		in:        bufio.NewScanner(in),
+		out:       f,
+		tee:       tee,
+	}, nil
+}
+
+// Run begins reading lines from the input and rotating logs as necessary.
+func (r *Rotator) Run() error {
+	for r.in.Scan() {
+		if r.size >= r.threshold {
+			if err := r.rotate(); err != nil {
+				return err
+			}
+		}
+
+		line := r.in.Bytes()
+
+		n, _ := r.out.Write(line)
+		m, _ := r.out.Write([]byte{'\n'})
+
+		if r.tee {
+			os.Stdout.Write(line)
+			os.Stdout.Write([]byte{'\n'})
+		}
+
+		r.size += int64(n + m)
+	}
+
+	return nil
+}
+
+// Close closes the output logfile.
+func (r *Rotator) Close() error {
+	return r.out.Close()
+}
+
+func (r *Rotator) rotate() error {
+	basename := strings.TrimSuffix(filepath.Base(r.filename), filepath.Ext(r.filename))
+	dir := filepath.Dir(r.filename)
+
+	now := time.Now()
+	arcName := fmt.Sprintf("%s %s.%3d.gz", basename, now.Format("2006-01-02 15.04.05"), now.Nanosecond()/int(1e6))
+	arcPath := filepath.Join(dir, arcName)
+
+	arc, err := os.OpenFile(arcPath, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	r.out.Seek(0, os.SEEK_SET)
+	z := gzip.NewWriter(arc)
+	if _, err = io.Copy(z, r.out); err != nil {
+		return err
+	}
+	if err = z.Close(); err != nil {
+		return err
+	}
+	if err := arc.Close(); err != nil {
+		return err
+	}
+	if err = r.out.Close(); err != nil {
+		return err
+	}
+
+	r.out, err = os.OpenFile(r.filename, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+
+	r.size = 0
+
+	return nil
+}
